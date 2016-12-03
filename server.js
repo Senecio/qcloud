1590a6c5742ecc899f4437c5ec90aa190685cd91
lishihai <jpweiyi@163.com>
2016-12-03 23:59:52 +0800

    5

--- server.js
+++ server.js
@@ -6,10 +6,23 @@ AV.init({
   masterKey: process.env.LEANCLOUD_APP_MASTER_KEY || 'pwpLDmVHzBGdihDpME24yEX8'
 });*/
 
+var os = require('os');  
+var IPv4,hostName;
+hostName=os.hostname();
+for(var i=0;i<os.networkInterfaces().en0.length;i++){  
+    if(os.networkInterfaces().en0[i].family=='IPv4'){  
+        IPv4=os.networkInterfaces().en0[i].address;  
+    }  
+}  
+console.log('----------local IP: '+IPv4);  
+console.log('----------local host: '+hostName);  
+
+
 var http = require("http");
 http.createServer(function(request, response) {  
     response.writeHead(200, {"Content-Type": "text/plain"});  
-    response.write("Hello World 123");  
+    response.write(IPv4);
+    response.write(hostName);
     response.end();
 }).listen(process.env.LEANCLOUD_APP_PORT);
 console.log("nodejs start listen process.env.LEANCLOUD_APP_PORT port!");
