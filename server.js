d9f67a17e08f7d7f2dfba116827cf0ce11f71ffc
lishihai <jpweiyi@163.com>
2016-12-03 13:50:24 +0800

    2

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+
+var AV = require('leanengine');
+AV.init({
+  appId: process.env.LEANCLOUD_APP_ID || 'KbDLPWwEVNtxnGYQn6XmkJF6-9Nh9j0Va',
+  appKey: process.env.LEANCLOUD_APP_KEY || 'Y0Ma4uJYsgHBX7uQBMMby5sp',
+  masterKey: process.env.LEANCLOUD_APP_MASTER_KEY || 'pwpLDmVHzBGdihDpME24yEX8'
+});
+
 var http = require("http");
 http.createServer(function(request, response) {  
     response.writeHead(200, {"Content-Type": "text/plain"});  
